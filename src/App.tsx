1e416a8f34c69a2581b79e9dd55f174b43d23d25
khorshedjhoni <khorshedjhoni@gmail.com>
2026-09-11 10:56:56 +0600

    Navbar add

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Navbar from './Components/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <div className='container mx-auto px-14 mt-2'>
+         <Navbar></Navbar>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import img from "../assets/logo-text.png";
+
+function Navbar(props) {
+  return (
+    <div  className="sticky top-0 z-50 bg-white shadow-md">
+        <div className="flex justify-between items-center gap-x-2">
+      <div>
+        <img src={img} alt="" />
+      </div>
+      <div>
+        <ul className=" flex justify-between items-center gap-x-4 font-semibold">
+          <li>Home</li>
+          <li>Technologies</li>
+          <li>Projects</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <div>
+        <input type="submit" value="Sign in" className="btn" />
+        <button className="btn  btn-secondary rounded-2xl">Sign Up</button>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default Navbar;
